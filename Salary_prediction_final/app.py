50118802469b27611fcd52062427c1dab57a49e2
soumyadeep92 <83584344+soumyadeep92@users.noreply.github.com>
2022-09-27 19:29:28 +0530

    Add files via upload

---
+++ Salary_prediction_final/app.py
@@ -0,0 +1,36 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    features = [x for x in request.form.values()]
+    final_features = [np.array(features)]
+    prediction = model.predict(final_features)
+
+    output = np.round(prediction[0],2)[0]
+
+    return render_template('index.html', prediction_text='Salary is {}'.format(output))
+
+
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    data = request.get_json(force=True)
+    prediction = model.predict([np.array(list(data.values()))])
+
+    output = prediction[0]
+    return jsonify(output)
+
+if __name__ == "__main__":
+    try:
+        app.run(port=5000, debug=True)
+    except:
+        print("Server is exited unexpectedly. Please contact server admin.")
+
