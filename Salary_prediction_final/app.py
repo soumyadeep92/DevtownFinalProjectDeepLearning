121f4ce1f8bf6388050b9212d7c8a6d931667cef
soumyadeep92 <83584344+soumyadeep92@users.noreply.github.com>
2022-10-01 18:27:45 +0530

    Add files via upload

--- Salary_prediction_final/app.py
+++ Salary_prediction_final/app.py
@@ -11,22 +11,14 @@ def home():
 
 @app.route('/predict',methods=['POST'])
 def predict():
-    features = [x for x in request.form.values()]
-    final_features = [np.array(features)]
+    features = [float(x) for x in request.form.values()]
+    final_features = [features]
     prediction = model.predict(final_features)
 
     output = np.round(prediction[0],2)[0]
 
-    return render_template('index.html', prediction_text='Salary is {} dollars'.format(output))
+    return render_template('index.html', prediction_text='Predicted Salary is {} dollars'.format(output))
 
-
-@app.route('/predict_api',methods=['POST'])
-def predict_api():
-    data = request.get_json(force=True)
-    prediction = model.predict([np.array(list(data.values()))])
-
-    output = prediction[0]
-    return jsonify(output)
 
 if __name__ == "__main__":
     try:
